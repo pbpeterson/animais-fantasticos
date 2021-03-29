68c4b3d535dede9d7523e609b6a2f33f7d5b3daf
Peterson Pereira <petersonbozza7@gmail.com>
2021-03-29 01:23:41 -0300

    retorno-this-classes

--- js/modules/login.js
+++ js/modules/login.js
@@ -1,21 +1,40 @@
-export default function ativalModal() {
-  const login = document.querySelector('[data-modal="abrir"]');
-  const fechar = document.querySelector('[data-modal="fechar"]');
-  const modalContainer = document.querySelector('[data-modal="container"]');
-  function toggleModal(event) {
-    event.preventDefault();
-    modalContainer.classList.toggle('ativo');
+export default class Modal{
+  constructor(login, fechar, modalContainer){
+    this.login = document.querySelector(login);
+    this.fechar = document.querySelector(fechar);
+    this.modalContainer = document.querySelector(modalContainer);
+
+    this.eventToggleModal = this.eventToggleModal.bind(this)
+    this.cliqueForalModal = this.eventToggleModal.bind(this)
+
   }
 
-  function cliqueForalModal(event) {
-    if (event.target === this) {
-      toggleModal(event);
+
+  toggleModal() {
+    this.modalContainer.classList.toggle('ativo');
+  }
+
+  eventToggleModal(event){
+    event.preventDefault()
+    this.toggleModal()
+  }
+
+  cliqueForalModal(event) {
+    if (event.target === this.modalContainer) {
+      this.toggleModal(event);
     }
   }
 
-  if (login && fechar && modalContainer) {
-    login.addEventListener('click', toggleModal);
-    fechar.addEventListener('click', toggleModal);
-    modalContainer.addEventListener('click', cliqueForalModal);
+  addModalEvent(){
+    this.login.addEventListener('click', this.eventToggleModal);
+    this.fechar.addEventListener('click', this.eventToggleModal);
+    this.modalContainer.addEventListener('click', this.cliqueForalModal);
+  }
+
+  init(){
+    if (this.login && this.fechar && this.modalContainer) {
+      this.addModalEvent()
+    }
+    return this
   }
 }

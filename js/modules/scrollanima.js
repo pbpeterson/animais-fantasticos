0930b10e71d4f0d81d0afe0259720fa959d5299a
Peterson Pereira <petersonbozza7@gmail.com>
2021-03-29 23:06:42 -0300

    animacao-aoscroll-refeita

---
+++ js/modules/scrollanima.js
@@ -0,0 +1,38 @@
+export default class ScrollAnima{
+
+  constructor(sections){
+    this.sections= document.querySelectorAll(sections);
+    this.windowsMetade = window.innerHeight * 0.6;
+    this.checkDistance = this.checkDistance.bind(this)
+  }
+
+  getDistance(){
+    this.distance= [...this.sections].map((section)=>{
+      const offset = section.offsetTop;
+      return{
+        element: section,
+        offset: offset - this.windowsMetade
+      };
+    });
+  }
+
+  checkDistance(){
+    this.distance.forEach((item) => {
+      if(window.pageYOffset > item.offset){
+        item.element.classList.add('ativo');
+      } else if (item.element.classList.contains('ativo')) {
+        item.element.classList.remove('ativo');
+      }
+    })
+  }
+
+  init(){
+    if(this.sections.length){
+      this.getDistance()
+      this.checkDistance()
+      window.addEventListener('scroll', this.checkDistance);
+      
+    }
+    return this
+  }
+}

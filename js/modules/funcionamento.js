c67d55ffd57936a75315133974ebd6339af8c9f3
Peterson Pereira <petersonbozza7@gmail.com>
2021-04-01 03:42:52 -0300

    adicionado slide de fotos

--- js/modules/funcionamento.js
+++ js/modules/funcionamento.js
@@ -1,40 +1,39 @@
 export default class Funcionamento {
-
-  constructor(funcionamento, activeClass){
-    this.funcionamento = document.querySelector(funcionamento)
+  constructor(funcionamento, activeClass) {
+    this.funcionamento = document.querySelector(funcionamento);
     this.activeClass = activeClass;
   }
-  
-  dadosFuncionamento(){
-  this.diasAberto = this.funcionamento.dataset.semana.split(',').map(Number);
-  this.horaAberto = this.funcionamento.dataset.horario.split(',').map(Number);
+
+  dadosFuncionamento() {
+    this.diasAberto = this.funcionamento.dataset.semana.split(',').map(Number);
+    this.horaAberto = this.funcionamento.dataset.horario.split(',').map(Number);
   }
 
-  dadosAgora(){
+  dadosAgora() {
     this.dataHoje = new Date();
     this.diaHoje = this.dataHoje.getDay();
-    this.horaHoje = this.dataHoje.getUTCHours() -3;
+    this.horaHoje = this.dataHoje.getUTCHours() - 3;
   }
 
-  estaAberto(){
+  estaAberto() {
     const aBerto = this.diasAberto.indexOf(this.diaHoje) !== -1;
     const horaABrir = this.horaHoje >= this.horaAberto[0] && this.horaHoje < this.horaAberto[1];
 
-    return aBerto && horaABrir
+    return aBerto && horaABrir;
   }
 
-  ativaAberto(){
+  ativaAberto() {
     if (this.estaAberto()) {
-    this.funcionamento.classList.add(activeClass);  
+      this.funcionamento.classList.add(this.activeClass);
     }
   }
 
-  init(){
-    if(this.funcionamento){
-    this.dadosFuncionamento();
-    this.dadosAgora();
-    this.ativaAberto();
+  init() {
+    if (this.funcionamento) {
+      this.dadosFuncionamento();
+      this.dadosAgora();
+      this.ativaAberto();
     }
-    return this
+    return this;
   }
 }
--- js/script.js
+++ js/script.js
@@ -1,42 +1,46 @@
-import ScrollSuave from './modules/scrollsuave.js'
-import Accordion from './modules/accordion.js'
-import ScrollAnima from './modules/scrollanima.js'
-import animaTextosImg from './modules/animaTexto.js'
-import ativalModal from './modules/login.js'
-import Tooltip from './modules/tooltip.js'
-import droopdownMenu from './modules/droopdownMenu.js'
-import menuMob from './modules/menu-mobile.js'
-import Funcionamento from './modules/funcionamento.js'
-import fetchanimais from './modules/fetchanimais.js'
-import fecthBTC from './modules/fetchBTC.js'
+import ScrollSuave from './modules/scrollsuave.js';
+import Accordion from './modules/accordion.js';
+import ScrollAnima from './modules/scrollanima.js';
+import animaTextosImg from './modules/animaTexto.js';
+import ativalModal from './modules/login.js';
+import Tooltip from './modules/tooltip.js';
+import droopdownMenu from './modules/droopdownMenu.js';
+import menuMob from './modules/menu-mobile.js';
+import Funcionamento from './modules/funcionamento.js';
+import fetchanimais from './modules/fetchanimais.js';
+import fecthBTC from './modules/fetchBTC.js';
+import SlideNav from './modules/slide.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
-scrollSuave.init()
+scrollSuave.init();
 
-const accordion = new Accordion('[data-anima="accordion"] dt')
-accordion.init()
+const accordion = new Accordion('[data-anima="accordion"] dt');
+accordion.init();
 
-const animatextos = new animaTextosImg('[data-tab="menu"] li', '[data-tab="content"] section')
-animatextos.init()
+const animatextos = new animaTextosImg('[data-tab="menu"] li', '[data-tab="content"] section');
+animatextos.init();
 
-const tooltip = new Tooltip('[data-tooltip]')
-tooltip.init()
+const tooltip = new Tooltip('[data-tooltip]');
+tooltip.init();
 
-const scrollanima = new ScrollAnima('[data-anime="scroll"]')
-scrollanima.init()
+const scrollanima = new ScrollAnima('[data-anime="scroll"]');
+scrollanima.init();
 
-const droopdownmenu = new droopdownMenu('[data-droopdown]')
-droopdownmenu.init()
+const droopdownmenu = new droopdownMenu('[data-droopdown]');
+droopdownmenu.init();
 
-const menumobile = new menuMob('[data-menu="button"]', '[data-menu="list"]')
-menumobile.init()
+const menumobile = new menuMob('[data-menu="button"]', '[data-menu="list"]');
+menumobile.init();
 
-const funcionamento = new Funcionamento('[data-semana]', 'aberto')
-funcionamento.init()
-
+const funcionamento = new Funcionamento('[data-semana]', 'aberto');
+funcionamento.init();
 
 ativalModal();
 
 fecthBTC('https://blockchain.info/ticker', '.btc-preco');
 
-fetchanimais('../animaisapi.json', '.numeros-grid')
+fetchanimais('../animaisapi.json', '.numeros-grid');
+
+const slide = new SlideNav('.slide', '.wrapper');
+slide.init();
+slide.addControl('.custom-controls');
